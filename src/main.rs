7a4004344d998242e7f48fe6b3e75022951c42a0
foxkill <foxkill@gmx.de>
2024-03-09 14:38:18 +0100

    Add close button.

--- src/main.rs
+++ src/main.rs
@@ -2,14 +2,20 @@
 
 
 pub mod components;
+use std::rc::Rc;
+
 use components::connect_cusip_handler;
+use slint::VecModel;
 
 slint::include_modules!();
 
 fn main() -> Result<(), slint::PlatformError> {
-    let ui: AppWindow = AppWindow::new()?;
+    let ui: AppWindow = AppWindow::new()?; 
 
+    ui.set_rows(Rc::new(VecModel::default()).into());
     connect_cusip_handler(&ui);
 
+    let weak = ui.as_weak();
+    ui.on_close(move || weak.unwrap().window().hide().unwrap());
     ui.run()
 }
