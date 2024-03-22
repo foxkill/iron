85d4a65a8ecf720dd3b30e749b366ef03966abe5
foxkill <foxkill@gmx.de>
2024-03-22 19:14:05 +0100

    Complete the model for delivering the chart data.

--- iron/src/components/qualitymodel.rs
+++ iron/src/components/qualitymodel.rs
@@ -1,21 +1,32 @@
 // !# Delivers the quality tab data.
 
-use std::str::FromStr;
+use std::{fmt::Display, str::FromStr};
 
-use auctionresult::{tenor::Tenor, treasury::{AuctionResultError, Treasury, TreasuryAccess}, Latest, SecurityType};
-use chrono::NaiveDateTime;
+use auctionresult::{
+    tenor::Tenor,
+    treasury::{security_type, AuctionResultError, Treasury, TreasuryAccess},
+    Latest, SecurityType,
+};
+use chrono::{Months, NaiveDateTime, Utc};
 
-#[derive(Debug)]
+#[derive(Debug, PartialEq, PartialOrd, Default)]
 pub struct DataPair {
-    x_axis: f64,
-    y_axis: NaiveDateTime,
+    pub x_axis: f64,
+    pub y_axis: NaiveDateTime,
 }
+
+// impl Default for DataPair {
+//     fn default() -> Self {
+//         Self { x_axis: Default::default(), y_axis: Default::default() }
+//     }
+// }
 
 pub enum TakeDown {
     BidToCover,
     PrimaryDealers,
     Indirects,
     Directs,
+    None,
 }
 
 impl FromStr for TakeDown {
@@ -34,32 +45,101 @@ impl FromStr for TakeDown {
     }
 }
 
+impl From<i32> for TakeDown {
+    fn from(value: i32) -> Self {
+        match value {
+            0 => TakeDown::BidToCover,
+            1 => TakeDown::PrimaryDealers,
+            2 => TakeDown::Indirects,
+            3 => TakeDown::Directs,
+            _ => TakeDown::None,
+        }
+    }
+}
+
+impl Display for TakeDown {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let str_repr = match self {
+            TakeDown::BidToCover => "Bid To Cover",
+            TakeDown::PrimaryDealers => "Primary Dealers",
+            TakeDown::Indirects => "Indirects",
+            TakeDown::Directs => "Directs",
+            TakeDown::None => "None",
+        };
+
+        write!(f, "{}", str_repr)
+    }
+}
+
 pub struct QualityModel;
 
 impl QualityModel {
-    pub fn query(security_type: SecurityType, lookback: usize, takedown: TakeDown) -> Result<Vec<DataPair>, AuctionResultError> {
-        let tenor = Tenor::default();
-        let latest_command = Latest::new(security_type, lookback, tenor);
+    pub fn query(
+        auction_type: impl Into<String>,
+        lookback: impl Into<String>,
+        takedown: TakeDown,
+    ) -> Result<Vec<DataPair>, AuctionResultError> {
+        let timerange = lookback.into().parse::<u32>().unwrap();
+
+        let now = Utc::now();
+
+        let at_that_time = now.checked_sub_months(Months::new(timerange));
+        let diff = now.with_timezone(&Utc) - at_that_time.unwrap().with_timezone(&Utc);
+        let days = diff.num_days() as usize;
+
+        let stype = auction_type.into();
+
+        // Get the tenor from the auction type.
+        let tenor = Tenor::parse(stype.to_owned())?;
+        // Get the security type from the auction type.
+
+        let st = QualityModel::to_security_type(stype)?;
+
+        let latest_command = Latest::new(st, days, tenor);
 
         let result = latest_command.get();
 
-        let Ok(treasuries) = result else {  
+        let Ok(treasuries) = result else {
             return Err(result.unwrap_err());
         };
 
-        Ok(vec![])
+        let fptr = match takedown {
+            TakeDown::BidToCover => |value: &Treasury| DataPair {
+                x_axis: value.get_bid_to_cover_ratio(),
+                y_axis: value.get_issue_date(),
+            },
+            TakeDown::PrimaryDealers => |value: &Treasury| DataPair {
+                x_axis: value.get_percentage_debt_purchased_by_dealers(),
+                y_axis: value.get_issue_date(),
+            },
+            TakeDown::Indirects => |value: &Treasury| DataPair {
+                x_axis: value.get_percentage_debt_purchased_by_indirects(),
+                y_axis: value.get_issue_date(),
+            },
+            TakeDown::Directs => |value: &Treasury| DataPair {
+                x_axis: value.get_percentage_debt_purchased_by_directs(),
+                y_axis: value.get_issue_date(),
+            },
+            TakeDown::None => |value: &Treasury| DataPair {
+                x_axis: 0.,
+                y_axis: value.get_issue_date(),
+            },
+        };
 
-        // let fptr = match takedown {
-        //     TakeDown::BidToCover => |value: &Treasury| { 
-        //         value.get_bid_to_cover_ratio()
-        //     }
-        //     TakeDown::PrimaryDealers => |value: &Treasury| value.get_bid_to_cover_ratio(),
-        //     TakeDown::Indirects => |value: &Treasury| value.get_bid_to_cover_ratio(),
-        //     Takedown::Directs => |value: &Treasury| value.get_bid_to_cover_ratio(),
-        // }
+        let result = treasuries.iter().map(fptr).collect::<Vec<_>>();
 
-        // let result = treasuries.iter().map(fptr).collect::<Vec<_>>();
+        Ok(result)
+    }
 
+    fn to_security_type(security_type: impl Into<String>) -> Result<SecurityType, AuctionResultError> {
+        let tenor = Tenor::parse(security_type)?;
+        let security = match (tenor.security(), tenor.shortcut()) {
+            (20..=30, "y") => SecurityType::Bond,
+            (2..=10, "y") => SecurityType::Note,
+            _ => return Err(AuctionResultError::NoTreasury),
+        };
+
+        Ok(security)
     }
 }
 
@@ -76,11 +156,9 @@ mod tests {
         println!("{:#?}", result);
         let btc = result
             .iter()
-            .map(|value| {
-                DataPair {
-                    x_axis: value.get_bid_to_cover_ratio(),
-                    y_axis: value.get_issue_date(),
-                }
+            .map(|value| DataPair {
+                x_axis: value.get_bid_to_cover_ratio(),
+                y_axis: value.get_issue_date(),
             })
             .collect::<Vec<_>>();
         println!("{:?}", btc);
